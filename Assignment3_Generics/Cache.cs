cbd1e3de5ed96d460bdfc34bec117f877b634d81
Basant Refaat <b.refaat@coreations.com>
2025-05-27 15:56:28 +0300

    conti in cache

--- Assignment3_Generics/Cache.cs
+++ Assignment3_Generics/Cache.cs
@@ -7,9 +7,25 @@ using System.Threading.Tasks;
 namespace Assignment3_Generics
 {
 
-    public class Cache <Tkey,Tvalue>
+    public class Cache <TKey, TValue> where TKey : notnull
     {
-        private readonly int _maxSize;
+        private int capacity;
+        private Tuple<TKey, TValue>[] items;
+        private int[] usageOrder;
+        private int count;
 
+        public Cache(int size)
+        {
+            capacity = size;
+            items = new Tuple<TKey, TValue>[capacity];
+            usageOrder = new int[capacity];
+            count = 0;
+        }
+        public void Add(TKey key, TValue value) 
+        {
+        
+        }
+
+        
     }
 }
